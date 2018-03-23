67916e803b2006564d6a6b0f457be824eee7f113
Roman Avila <avilaroman@gmail.com>
2018-03-23 01:44:10 -0300

    Create forminteligente.php

---
+++ forminteligente.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+   <!-- HTML meta refresco URL de redireccion -->
+   <meta http-equiv="refresh"
+   content="0; url=https://www.inteligente.com.ar/api/informes/solicitar/<?php echo $_GET["cuil"]; ?>?username=gbellanza&password=schneider16">
+</head>
+<body>
+
+Resolviendo
+
+</body>
+</html>
