29454e7485e728e37da44b1caa6bd75e2bba9b4e
Roman Avila <avilaroman@gmail.com>
2018-03-23 01:10:58 -0300

    echo

--- inteligente.php
+++ inteligente.php
@@ -1,2 +1,4 @@
+<?php 
 $informe = json_decode(file_get_contents('https://www.inteligente.com.ar/api/informes/solicitar/:20317656506?username={gbellanza}&password={schneider16}'));
-echo $informe;
+echo $informe; 
+?>
